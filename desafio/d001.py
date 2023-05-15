6fc8c0537b8c995039772c115b64f816f0572b7b
Cesar Ricardo <48608170+huque44@users.noreply.github.com>
2023-05-15 10:58:26 -0300

    Initial commit

---
+++ desafio/d001.py
@@ -0,0 +1,2 @@
+nome = input('Qual é o seu nome? ')
+print(nome+'! Prazer em te conhecer.')
---
+++ desafio/d002.py
@@ -0,0 +1,5 @@
+d = input('Dia: ')
+m = input('Mês: ')
+a = input('Ano: ')
+nome = input('Digite seu nome: ')
+print('{} nasceu no dia {} de {} de {}.'.format(nome, d, m, a))
